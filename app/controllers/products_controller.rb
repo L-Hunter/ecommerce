26d01061ffe778096c1d123eb3d22aa8b6512083
TheButlerDidIt <labutler@gmail.com>
2017-01-08 11:48:46 -0500

    close #3 creates working search box

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,11 @@ before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
+    if params[:search]
+      @products = Product.search(params[:search]).order("created_at DESC")
+    else
+      @products = Product.all.order("created_at DESC")
+    end
   end
 
   def show
