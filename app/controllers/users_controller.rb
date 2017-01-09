514424b2593888c8b297724b7d2dc96d718795c4
TheButlerDidIt <labutler@gmail.com>
2017-01-09 15:45:47 -0500

    adds partial form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
+    @user = User.new
     # current_user = User.find(session[:user_id])
   end
 
@@ -20,7 +21,14 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
     @user = User.create(user_params)
     Wishlist.create(user_id: @user.id)
     Cart.create(user_id: @user.id)
-    redirect_to @user
+    # redirect_to @user
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash[:notice] = "Error Saving Account."
+      render new_user_path
+    end
   end
 
   def edit
@@ -39,7 +47,7 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
 private
 
   def user_params
-    params.require(:user).permit(:fname, :lname, :email, :password, :ccnum, :cctype, :street, :city)
+    params.require(:user).permit(:fname, :lname, :email, :password, :ccnum, :cctype, :street, :city, :password_confirmation)
   end
 
   def set_user
